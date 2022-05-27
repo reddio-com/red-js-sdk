b164ca53df74994627875f681a15172f28df0f25
xuwu <zx597813039@gmail.com>
2022-05-27 10:25:43 +0800

    feat: add getAssetType func and unit test

--- src/utils/asset.test.ts
+++ src/utils/asset.test.ts
@@ -1,4 +1,4 @@
-import { getAssetInfo } from './asset';
+import { getAssetInfo, getAssetType } from './asset';
 import { Type } from './enum';
 
 describe('getAssetInfo', () => {
@@ -15,5 +15,25 @@ describe('getAssetInfo', () => {
       '0xb8b8667200000000000000000000000020a36B174dfb726A33a8416eD2E4894719236140'
     );
     expect(getAssetInfo('' as Type, tokenAddress)).toEqual(null);
+  });
+});
+
+describe('getAssetType', () => {
+  it('should get current type', function() {
+    const tokenAddress = '0x20a36B174dfb726A33a8416eD2E4894719236140';
+    const quantum = 1;
+    expect(getAssetType(Type.ETH, tokenAddress, quantum)).toEqual(
+      '0x1142460171646987f20c714eda4b92812b22b811f56f27130937c267e29bd9e'
+    );
+    console.log(getAssetType(Type.ETH, tokenAddress, quantum));
+    expect(getAssetType(Type.ERC20, tokenAddress, quantum)).toEqual(
+      '0x11ab3af6727fcd73565816e286a2ee5008c230ac36a9d7f8de9cc9494bb95d'
+    );
+    expect(getAssetType(Type.ERC721, tokenAddress, quantum)).toEqual(
+      '0x39e2cce8afb88142a6affa0da853055331826b7f8ddc9ef890edc4dd57ee07f'
+    );
+    expect(getAssetType(Type.ERC721M, tokenAddress, quantum)).toEqual(
+      '0xe4e7390a3d8762570fce723533098772f6e0d575a54420e39296a13c525f23'
+    );
   });
 });
--- src/utils/asset.ts
+++ src/utils/asset.ts
@@ -1,4 +1,5 @@
-// import { ethers } from 'ethers';
+import { BigNumber, ethers } from 'ethers';
+import BN from 'bn.js';
 import { Type } from './enum';
 
 export const getAssetInfo = (type: Type, address: string) => {
@@ -20,4 +21,25 @@ export const getAssetInfo = (type: Type, address: string) => {
       return null;
     }
   }
+};
+
+export const getAssetType = (type: Type, address: string, quantum: number) => {
+  const assetInfo = getAssetInfo(type, address);
+  const value = new BN(
+    BigNumber.from(
+      ethers.utils.solidityKeccak256(['bytes', 'uint256'], [assetInfo, quantum])
+    ).toString(),
+    10
+  );
+  return (
+    '0x' +
+    value
+      .and(
+        new BN(
+          '03FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF',
+          16
+        )
+      )
+      .toString('hex')
+  );
 };
