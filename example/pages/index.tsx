0d041cb03b7c9c1b5fd70db2cf4e437333283e5d
KieSun <zx597813039@gmail.com>
2022-06-14 18:19:44 +0800

    chore: deploy setting

--- example/pages/index.tsx
+++ example/pages/index.tsx
@@ -1,6 +1,5 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
 import Link from 'next/link'
 import {Button, Text, Spacer} from '@nextui-org/react';
 import styles from '../styles/Home.module.css'
@@ -61,19 +60,6 @@ const Home: NextPage = () => {
           </Link>
         </div>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
   )
 }
