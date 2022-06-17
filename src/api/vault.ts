9878854d582d863e8189d5a6567da6a378d4b914
KieSun <zx597813039@gmail.com>
2022-06-17 12:35:34 +0800

    feat: vault api

--- src/api/vault.ts
+++ src/api/vault.ts
@@ -3,6 +3,9 @@ import { Response, VaultParams, VaultResponse } from '../types';
 import { parseParams } from '../utils';
 
 const getVaultID = async (request: AxiosInstance, params: VaultParams) => {
+  if (Array.isArray(params.starkKey)) {
+    params.starkKey = params.starkKey.join(',');
+  }
   return request.get<Response<VaultResponse>>('/api/v1/vault', {
     params: {
       ...parseParams(params),
