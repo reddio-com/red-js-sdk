e657c497354a89968d5b21e10ddcb74bad3bfac8
KieSun <zx597813039@gmail.com>
2022-06-17 13:14:39 +0800

    feat: deposit

--- src/api/deposit.ts
+++ src/api/deposit.ts
@@ -13,7 +13,12 @@ export const depositERC20 = (
       const signer = provider.getSigner();
       const contract = new ethers.Contract(contractAddress, abi, signer);
       const { starkKey, vaultId, quantizedAmount, assetType } = params;
-      contract.depositERC20(starkKey, assetType, vaultId, quantizedAmount);
+      contract.depositERC20(
+        starkKey,
+        assetType,
+        vaultId,
+        ethers.utils.parseUnits(quantizedAmount.toString(), 6)
+      );
 
       contract.on('LogDeposit', (...args) => {
         resolve(args);
@@ -34,8 +39,9 @@ export const depositETH = (
       const signer = provider.getSigner();
       const contract = new ethers.Contract(contractAddress, abi, signer);
       const { starkKey, vaultId, assetType, quantizedAmount } = params;
+      const amount = Math.floor(Number(quantizedAmount) * 1000000) / 1000000;
       contract.depositEth(starkKey, assetType, vaultId, {
-        value: ethers.utils.parseEther(quantizedAmount.toString()),
+        value: ethers.utils.parseEther(amount.toString()),
       });
 
       contract.on('LogDeposit', (...args) => {
