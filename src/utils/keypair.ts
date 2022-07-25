9a129ea2f0ceb908a3afd6b5c805cbd8f4ddc583
KieSun <zx597813039@gmail.com>
2022-07-25 20:45:37 +0800

    feat: keyDerivation msgParams

--- src/utils/keypair.ts
+++ src/utils/keypair.ts
@@ -4,10 +4,27 @@ import { JsonRpcProvider } from '@ethersproject/providers';
 
 export const generateFromEthSignature = async (
   provider: JsonRpcProvider,
-  msgParams: any
+  content: string | Record<string, any>
 ) => {
   const method = 'eth_signTypedData_v4';
   const from = await provider.getSigner().getAddress();
+  const value =
+    typeof content === 'string'
+      ? {
+          domain: {
+            chainId: 5,
+          },
+          message: {
+            contents: content,
+          },
+          primaryType: 'reddio',
+          types: {
+            EIP712Domain: [{ name: 'chainId', type: 'uint256' }],
+            reddio: [{ name: 'contents', type: 'string' }],
+          },
+        }
+      : JSON.stringify(content);
+  const msgParams = JSON.stringify(value);
   const result = await provider.send(method, [from, msgParams]);
   const privateKey = keyDerivation.getPrivateKeyFromEthSignature(result);
   const publicKey = '0x' + keyDerivation.privateToStarkKey(privateKey);
