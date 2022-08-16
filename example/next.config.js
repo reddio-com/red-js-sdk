e0eb2e4c9a951a31439968cd5e6cff0dce74777e
KieSun <zx597813039@gmail.com>
2022-08-16 19:09:58 +0800

    chore: build

--- example/next.config.js
+++ example/next.config.js
@@ -7,6 +7,9 @@ const nextConfig = {
     config.resolve.alias['@reddio/js-sdk'] = path.resolve('../dist/index.js');
     return config;
   },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
 
 module.exports = nextConfig;
