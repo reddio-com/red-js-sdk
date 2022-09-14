8da1a31cb24caf19c64a53ddd55f46aee1f209ba
KieSun <zx597813039@gmail.com>
2022-09-14 16:28:27 +0800

    feat: reduce the number of order interface requests

--- src/api/order.ts
+++ src/api/order.ts
@@ -4,7 +4,9 @@ import {
   OrderInfoResponse,
   OrderRequestParams,
   OrderResponse,
+  OrderInfoRequestParams,
 } from '../types';
+import { parseParams } from '../utils';
 
 export async function order(
   request: AxiosInstance,
@@ -15,6 +17,13 @@ export async function order(
   });
 }
 
-export async function info(request: AxiosInstance) {
-  return request.get<Response<OrderInfoResponse>>('/v1/order/info');
+export async function info(
+  request: AxiosInstance,
+  params: OrderInfoRequestParams
+) {
+  return request.get<Response<OrderInfoResponse>>('/v1/order/info', {
+    params: {
+      ...parseParams(params),
+    },
+  });
 }
